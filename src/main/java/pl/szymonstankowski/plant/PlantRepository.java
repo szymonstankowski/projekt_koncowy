350ee04b4a51486e30f24fb02e630b43cea3f675
szymonstankowski <szymonstankowski1@gmail.com>
2021-10-25 21:51:15 +0200

    dalsze poprawki

--- src/main/java/pl/szymonstankowski/plant/PlantRepository.java
+++ src/main/java/pl/szymonstankowski/plant/PlantRepository.java
@@ -2,10 +2,7 @@ package pl.szymonstankowski.plant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PlantRepository extends JpaRepository<Plant, Long> {
 
-    List<Plant> findAll();
 
 }
