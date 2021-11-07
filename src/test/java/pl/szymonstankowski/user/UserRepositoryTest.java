841982139ebf360184288b613ec460f4498bc323
szymonstankowski <szymonstankowski1@gmail.com>
2021-11-07 23:32:37 +0100

    tests development

--- src/test/java/pl/szymonstankowski/user/UserRepositoryTest.java
+++ src/test/java/pl/szymonstankowski/user/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package pl.szymonstankowski.user;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -37,24 +38,87 @@ class UserRepositoryTest {
         Optional<User> expected = underTest.findUserByName(name);
         //then
         assertThat(expected).isEmpty();
-
-
     }
 
 
     @Test
-    void findUserByEmail() {
+    void itShouldFindUserByEmail() {
+        //given
+        List<UserPlants> userPlants = new ArrayList<>();
+        String email = "szymon@wp.pl";
+        User user = new User(
+                "szymon",
+                email,
+                "pass",
+                "ADMIN_ROLE",
+                false,
+                false,
+                userPlants);
+
+        underTest.save(user);
+
+        //when
+        Optional<User> expected = underTest.findUserByName(email);
+        //then
+        assertThat(expected).isEmpty();
     }
 
     @Test
-    void findUserById() {
+    void itShouldFindUserById() {
+        //given
+        List<UserPlants> userPlants = new ArrayList<>();
+
+        User user = new User(
+                "szymon",
+                "szymon@wp.pl",
+                "pass",
+                "ADMIN_ROLE",
+                false,
+                false,
+                userPlants);
+
+        underTest.save(user);
+        Long id = user.getId();
+
+        //when
+        User expected = underTest.findUserById(id);
+
+        //then
+        assertThat(expected).isEqualTo(user);
+
     }
 
     @Test
+    @Disabled
     void deleteById() {
     }
 
     @Test
-    void enableUser() {
+    void itShouldFindIfUserIsEnabled() {
+        //given
+        List<UserPlants> userPlants = new ArrayList<>();
+        User user = new User(
+                "szymon",
+                "szymon@wp.pl",
+                "pass",
+                "ADMIN_ROLE",
+                false,
+                false,
+                userPlants);
+
+        underTest.save(user);
+        Long id = user.getId();
+
+
+        //when
+        User userById = underTest.findUserById(id);
+        userById.setEnabled(true);
+        Boolean expected = userById.getEnabled();
+
+
+        //then
+        assertThat(expected).isTrue();
+
+
     }
 }
